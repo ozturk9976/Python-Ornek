648d37363f97dcbc4f1332cc9ae43294909a43d0
ozturk9976 <74475115+ozturk9976@users.noreply.github.com>
2022-06-05 20:49:26 +0300

    update

--- PythonOrnek/hello/PythonOrnek.py
+++ PythonOrnek/hello/PythonOrnek.py
@@ -1,24 +1,89 @@
+import random
+
 #OOP
 #class => person(name,surname,age,birthday,calculateAge())
 #instance (object)
 
-class calisan:
-    def __init__(self,name,surname,age): #self => oluşturulan değişkene otomatik gönderilir
-        print("__init__ fonksiyonu çalışıyor") #calisan1 = self
-        #attributes
-        self.name = name
-        self.surname = surname
-        self.age = age
-    def show_info(self):
-        print(f"Ad:{self.name} Soyad: {self.surname}  Yaş: {self.age}")
+# class calisan:
+#     def __init__(self,name,surname,age): #self => oluşturulan değişkene otomatik gönderilir
+#         print("__init__ fonksiyonu çalışıyor") #calisan1 = self
+#         #attributes
+#         self.name = name
+#         self.surname = surname
+#         self.age = age
+#     def show_info(self):
+#         print(f"Ad:{self.name} Soyad: {self.surname}  Yaş: {self.age}")
 
-calisan1 = calisan("Ali","Veli",10)
-print(calisan1.name,calisan1.surname,calisan1.age)      
+# calisan1 = calisan("Ali","Veli",10)
+# print(calisan1.name,calisan1.surname,calisan1.age)      
 
-calisan2 = calisan("Ayşe","Yıldız",35)
-print(calisan2.name,calisan2.surname,calisan2.age)
+# calisan2 = calisan("Ayşe","Yıldız",35)
+# print(calisan2.name,calisan2.surname,calisan2.age)
 
-calisan1.show_info()
+# calisan1.show_info()
+
+# calisan.show_info(calisan1)
+
+#ÖRNEK1
+#SAYI TAHMİN OYUNU
+#BİR TANE OYUNCU SINIFI OLUŞTURUN
+#OYUNCU SINIFININ PUAN,SAYI GİBİ DEĞİŞKENLERİ OLSUN
+#OYNA METODU OLSUN
+#BİLGİSAYARIN RASTGELE OLUŞTURDUĞU 1-10 ARASI SAYIYI HER SEFERİNDE
+#TAHMİN ETMEYE ÇALIŞSIN
+#BİLEMEZSE -5 PUAN BİLİRSE +50 PUAN KAZANSIN
+#OYUN 100 PUAN OLUNCA VEYA 0 PUAN OLUNCA BİTSİN
+#BAŞLANGIÇ PUANI FARKETMEZ
+
+
+class oyun:
+    def __init__(self,isim,puan,bpuan):
+        ##attributes
+        self.isimgirdiisim = isim
+        self.puan = puan
+        self.bpuan = bpuan
+        
+
+    isimgirdi = input("isim giriniz : ")
+
+    def zar1(self):
+         self.zar = random.randint(1,6)
+         print(self.zar)
+
+    def zar2(self):
+         self.zarGamer = random.randint(1,6)
+         print(self.zarGamer)
+
+
+    def puanla(self):
+            if self.zar < self.zarGamer:
+                self.bpuan +=10
+                self.puan -=5
+                print(self.puan,self.bpuan)
+
+            if self.zar > self.zarGamer:
+                self.puan += 10
+                self.bpuan -=10
+                print(self.puan,self.bpuan)
+
+oyun = oyun("Oyuncu",50,50)
+while True:
+   oyun.zar1()
+   oyun.zar2()
+   oyun.puanla()
+   if oyun.bpuan == 100 or oyun.puan == 0:
+       print("PC Kazandı")
+       break
+   if oyun.puan == 100 or oyun.bpuan == 0:
+       print(oyun.isimgirdiisim, "kazandı")
+       break
+
+
+
+
+
+
+
 
 
 
@@ -124,23 +189,3 @@ calisan1.show_info()
 
 
 # 2-Belirlenen kullanıcı adı ve şifre doğru girildiğinde "giriş başarılı" , yanlış girildiğinde "giriş başarısız" yazan program
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
